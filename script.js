0f4371cab117c853803791a7e7c899d6d533670d
“Nova3062  git config --global user.name “Nova3062 <zwemerj@gmail.com>
2023-06-24 15:23:04 -0600

    happy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const ENDPOINT_URL = "https://645d6729250a246ae31ed307.mockapi.io/KingsoftheRealm"
-let newId 
+const ENDPOINT_URL = "https://645d6729250a246ae31ed307.mockapi.io/KingsoftheRealm" //The code defines a constant ENDPOINT_URL which represents the URL of the API endpoint.
+let newId //variable newId declared in the global scope, which will be used to store the ID of the selected king for updating.
   
-function resetTable(){
+function resetTable(){//The resetTable() function is defined. It makes a GET request to the API endpoint using $.get(). Upon receiving the response, it removes all existing table cells (<td>) and appends new rows (<tr>) based on the retrieved data. Each row contains king information and buttons for updating and deleting.
     $.get(ENDPOINT_URL).then(data => {
         $('td').remove();
         data.map(king => {
@@ -22,7 +22,7 @@ function resetTable(){
     })
 }
 
-function addKing(){
+function addKing(){//The addKing() function is responsible for adding a new king to the API. It makes a POST request to the API endpoint with the form input values, and upon successful completion, it calls resetTable() to redraw the table and clears the form fields.
     $.post(ENDPOINT_URL, {
         kingName: $('#kingName').val(),
         nickName: $('#nickName').val(),
@@ -38,7 +38,7 @@ function addKing(){
 }
 
 
-function updateKing(newId){
+function updateKing(newId){//The updateKing() function is used to update a king's information. It sends a PUT request to the API endpoint with the updated data in JSON format. After the request is complete, it calls resetTable() to redraw the table.
     $.ajax(`${ENDPOINT_URL}/${newId}`, {
         method: "PUT",
         contentType: 'application/json',
@@ -52,14 +52,14 @@ function updateKing(newId){
         }),
     }).then(resetTable); 
 }
-$('#smashbutton').on('click', () => {
+$('#smashbutton').on('click', () => {//The $('#smashbutton').on('click') event handler is triggered when the update button is clicked inside the modal. It retrieves the ID of the selected king and calls the updateKing() function with the newId parameter.
     const id = $('#staticBackdrop').data('id');
     updateKing(newId);
     
  });
 
 
-function openmodal(id) {
+function openmodal(id) {//The openmodal() function is triggered when the update button is clicked. It makes a GET request to retrieve the specific king's information based on the provided ID. The retrieved data is then used to populate the modal form fields. The modal is displayed using Bootstrap's modal() method.
     newId = id
     $.ajax(`${ENDPOINT_URL}/${id}`, {
        method: 'GET',
@@ -77,7 +77,7 @@ function openmodal(id) {
  }
 
 
-const deleteKing = id => {
+const deleteKing = id => {//The deleteKing() function is responsible for deleting a king from the API. It sends a DELETE request to the API endpoint with the specified ID. After the deletion is completed, it calls resetTable() to redraw the table.
     $.ajax(`${ENDPOINT_URL}/${id}`, {
       method: 'DELETE',
     }) 
@@ -86,8 +86,10 @@ const deleteKing = id => {
 
 
 //draw table upon initial page load.
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {//The document.addEventListener('DOMContentLoaded') event is used to call the resetTable() function when the page is loaded. This ensures that the table is drawn with the existing king data.
+
     resetTable();
  });
 
 //  .then($("#addModal").modal("hide"));
+//Overall, this code sets up the necessary functions to interact with the API endpoint, handles adding, updating, and deleting kings, and updates the table in real-time when changes occur.
